f83f5a7e16a85066f67707a8699b4d7c0f303fdc
Helder Jordão <joh2brg@bosch.com>
2024-10-10 21:09:10 +0100

    #time 1h exemplo de definição de variáveis e print no ecrã

---
+++ src/examples/ex-01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    char inicial = 'A';
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Inicial: %c\n", inicial);
+
+    return 0;
+}
